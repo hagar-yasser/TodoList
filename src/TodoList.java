e4a27e63c73693727313df5cd146751002192c8d
hagar_yasser <hagaryasser642@gmail.com>
2022-10-19 16:25:32 +0200

    created TodoList Object

---
+++ src/TodoList.java
@@ -0,0 +1,5 @@
+public class TodoList {
+    private int currentSizeOfTodoItemsList=100;
+    private TodoItem[]todoItemsList=new TodoItem[currentSizeOfTodoItemsList];
+    private int indexOfLastItemInList=-1;
+}
