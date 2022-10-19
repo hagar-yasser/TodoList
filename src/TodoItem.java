026e1b0d7e1f15df67db12da9a7e36d76285f2ac
hagar_yasser <hagaryasser642@gmail.com>
2022-10-19 16:05:04 +0200

    added TodoItem Object

---
+++ src/TodoItem.java
@@ -0,0 +1,81 @@
+import java.time.LocalDate;
+
+public class TodoItem {
+    private String title;
+    private String description;
+    private String category;
+    private int priority;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private boolean favourite;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public boolean isFavourite() {
+        return favourite;
+    }
+
+    public void setFavourite(boolean favourite) {
+        this.favourite = favourite;
+    }
+
+    @Override
+    public String toString() {
+        return "TodoItem{" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", category='" + category + '\'' +
+                ", priority=" + priority +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                ", favourite=" + favourite +
+                '}';
+    }
+}
